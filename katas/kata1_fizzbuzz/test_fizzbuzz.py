e16ac5f74527898e3d6df03b19d482b73e705f48
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2026-04-06 11:44:40 -0600

    kata 1: test 4

--- katas/kata1_fizzbuzz/test_fizzbuzz.py
+++ katas/kata1_fizzbuzz/test_fizzbuzz.py
@@ -1,14 +1,15 @@
 import unittest
 from fizzbuzz import fizzbuzz
 
+
 class TestFizzBuzz(unittest.TestCase):
     """
     Tests for the fizzbuzz function.
     """
 
-    def test_should_return_number_as_string(self):
+    def test_should_return_number_as_string_when_number_is_not_multiple_of_3_or_5(self):
         """
-        Test that the function returns the number as a string
+        Test that the function returns the number as a string when it is not a multiple of 3 or 5
         """
 
         # Given
@@ -20,7 +21,7 @@ class TestFizzBuzz(unittest.TestCase):
         # Then
         self.assertEqual(result, "1")
 
-    def test_multiple_of_3_should_return_fizz(self):
+    def test_should_return_fizz_when_number_is_multiple_of_3(self):
         """
         Test that the function returns "Fizz" when the number is a multiple of 3
         """
@@ -34,7 +35,7 @@ class TestFizzBuzz(unittest.TestCase):
         # Then
         self.assertEqual(result, "Fizz")
 
-    def test_multiple_of_5_should_return_buzz(self):
+    def test_should_return_buzz_when_number_is_multiple_of_5(self):
         """
         Test that the function returns "Buzz" when the number is a multiple of 5
         """
@@ -48,6 +49,16 @@ class TestFizzBuzz(unittest.TestCase):
         # Then
         self.assertEqual(result, "Buzz")
 
+    def test_should_return_fizzbuzz_when_number_is_multiple_of_3_and_5(self):
+        """
+        Test that the function returns "FizzBuzz" when the number is a multiple of 3 and 5
+        """
 
-        
+        # Given
+        number = 15
 
+        # When
+        result = fizzbuzz(number)
+
+        # Then
+        self.assertEqual(result, "FizzBuzz")
