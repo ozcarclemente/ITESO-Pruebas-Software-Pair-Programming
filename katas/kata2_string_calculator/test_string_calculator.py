0aece9b2e8839fae871d331ebfd7dc035ceb6ad3
Emilio Maciel <emilio.maciel@iteso.mx>
2026-04-06 12:08:56 -0600

    kata 2: test 2

--- katas/kata2_string_calculator/test_string_calculator.py
+++ katas/kata2_string_calculator/test_string_calculator.py
@@ -49,4 +49,19 @@ class TestStringCalculator(unittest.TestCase):
         # Then
         self.assertEqual(result, 3)
 
+    
+    def test_should_allow_add_to_handle_an_unknown_number_of_arguments(self):
+        """
+        Test that the function can handle an unknown number of arguments
+        """
+
+        # Given
+        numbers = "1,2,3,4,5"
+
+        # When
+        result = add(numbers)
+
+        # Then
+        self.assertEqual(result, 15)
+
 
