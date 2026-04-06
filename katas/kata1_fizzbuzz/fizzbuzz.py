04d9b6c93a3537851e5631f0ada375171a87627f
Emilio Maciel <emilio.maciel@iteso.mx>
2026-04-06 11:46:47 -0600

    kata 1: exercise 4

--- katas/kata1_fizzbuzz/fizzbuzz.py
+++ katas/kata1_fizzbuzz/fizzbuzz.py
@@ -1,7 +1,13 @@
 
 
 def fizzbuzz(number: int) -> str:
-    
+    """
+    Returns "Fizz" if the number is a multiple of 3, "Buzz" if it is a multiple of 5, and "FizzBuzz" 
+    if it is a multiple of both 3 and 5. If the number is not a multiple of 3 or 5, it returns the number as a string.
+    """
+    if number % 5 == 0 and number % 3 == 0:
+        return "FizzBuzz"
+
     if number % 3 == 0:
         return "Fizz"
     
