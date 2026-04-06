bfc7cc7d5b8cf1f531678f90edaf0c7c08f946b3
Sofía Mercado Moreno <sofiamercadomoreno@Sofias-MacBook-Pro.local>
2026-04-06 12:09:47 -0600

    Kata 2 - Exercise 2

--- katas/kata2_string_calculator/string_calculator.py
+++ katas/kata2_string_calculator/string_calculator.py
@@ -3,6 +3,6 @@ def add(numbers: str) -> int:
     if numbers == "":
         return 0
     else:
-        a, b = numbers.split(",")
-        return int(a) + int(b)
+        return sum(map(int, numbers.split(",")))
+
 
