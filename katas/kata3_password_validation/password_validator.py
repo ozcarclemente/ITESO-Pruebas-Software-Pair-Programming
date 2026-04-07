be45ae46be8044f10f78ac5ea2af751902d69b3e
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2026-04-07 16:25:21 -0600

    Kata 3 - Exercise 3

--- katas/kata3_password_validation/password_validator.py
+++ katas/kata3_password_validation/password_validator.py
@@ -1,10 +1,15 @@
 
 def validate_password(password: str):
 
+    errors = []
+
     if len(password) < 8:
-        return "Password must be at least 8 characters long"
-    
+        errors.append("Password must be at least 8 characters long")
+
     if sum(c.isdigit() for c in password) < 2:
-        return "The password must contain at least 2 numbers"
-    
+        errors.append("The password must contain at least 2 numbers")
+
+    if errors:
+        return "\n".join(errors)
+
     return True
