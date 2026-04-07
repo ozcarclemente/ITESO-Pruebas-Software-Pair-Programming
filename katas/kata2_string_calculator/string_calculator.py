909d92e1bf5cf318b5c74bfaf4b8075cfcd10377
Emilio Maciel <emilio.maciel@iteso.mx>
2026-04-07 13:40:14 -0600

    kata 2: exercise 6

--- katas/kata2_string_calculator/string_calculator.py
+++ katas/kata2_string_calculator/string_calculator.py
@@ -3,17 +3,32 @@ def add(numbers: str) -> int:
         return 0
 
     delimiter = ","
+    data = numbers
+
     if numbers.startswith("//"):
-        header, numbers = numbers.split("\n", 1)
+        header, data = numbers.split("\n", 1)
         delimiter = header[2:]
+        
+        
+        if delimiter != "," and "," in data:
+            pos = data.find(",")
+            raise ValueError(f"'{delimiter}' expected but ',' found at position {pos}")
 
-    if numbers.endswith(delimiter):
+    if data.endswith(delimiter) or data.endswith("\n"):
         raise ValueError("Input cannot end with a separator")
+    
+    parts = data.replace("\n", delimiter).split(delimiter)
 
-    parts = numbers.replace("\n", delimiter).split(delimiter)
-
+    int_list = []
     for part in parts:
-        if not part.isdigit():
-            raise ValueError(f"'{delimiter}' expected but '{part}' found")
+        if not part: 
+            int_list.append(0)
+        else:
+            int_list.append(int(part))
 
-    return sum(int(p) for p in parts)
+    negatives = [n for n in int_list if n < 0]
+    if negatives:
+        neg_str = ",".join(map(str, negatives))
+        raise ValueError(f"Negative number(s) not allowed: {neg_str}")
+
+    return sum(int_list)
