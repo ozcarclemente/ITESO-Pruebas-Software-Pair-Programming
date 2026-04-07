8976e24ba8497eb15ee5f0fbb41668f87bd70842
Sofía Mercado Moreno <sofiamercadomoreno@Sofias-MacBook-Pro.local>
2026-04-07 16:11:17 -0600

    Kata 3 - Exercise 2

--- katas/kata3_password_validation/password_validator.py
+++ katas/kata3_password_validation/password_validator.py
@@ -4,4 +4,7 @@ def validate_password(password: str):
     if len(password) < 8:
         return "Password must be at least 8 characters long"
     
+    if sum(c.isdigit() for c in password) < 2:
+        return "The password must contain at least 2 numbers"
+    
     return True
